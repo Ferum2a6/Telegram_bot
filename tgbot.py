962a6eb15e10616a7160e9e974e4c86b9963507d
Ferum <105567525+Ferum2a6@users.noreply.github.com>
2022-12-20 00:08:09 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -18,6 +18,24 @@ def bot_hello(message):
 def bot_skills(message):
     message_bot = bot.send_message(message.chat.id, text_commands)
 
+@bot.message_handler(commands=['random_number'])
+def button_message(message):
+    mk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    b1 = types.KeyboardButton('1 - 10')
+    b2 = types.KeyboardButton('1 - 20')
+    b3 = types.KeyboardButton('1 - 30')
+    mk.add(b1, b2, b3)
+    bot.send_message(message.chat.id, 'Select range', reply_markup=mk)
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == '1 - 10':
+        bot.send_message(message.chat.id, f'Your number: {randint(1, 10)}') # import random!!
+    elif message.text == '1 - 20':
+        bot.send_message(message.chat.id, f'Your number: {randint(1, 20)}') # import random!!
+    elif message.text == '1 - 30':
+        bot.send_message(message.chat.id, f'Your number: {randint(1, 30)}') # import random!!
+    
 
 
 """Start"""
